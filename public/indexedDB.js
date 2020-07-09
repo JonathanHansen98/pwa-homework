a8ec9a0b46fb0c79a624a31b502ab7d26fccaa0b
Jonathan Hansen <kriah0872@gmail.com>
2020-07-09 15:28:00 -0700

    added mongo uri

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,25 +1,74 @@
 // Step - 1
-const dbRequest = indexedDB.open("toDoList_db", 1);
-
-// Step - 4: Initialization or whenever the version is changed.
+let db;
+const dbRequest = indexedDB.open("budget_db", 1);
 dbRequest.onupgradeneeded = (event) => {
-    console.log("Initializing database");
-    console.log(event.target);
+  console.log("Initializing database");
+  console.log(event.target);
 
-    const db = event.target.result;
+  const db = event.target.result;
 
-    const store = db.createObjectStore("transactions", { autoIncrement: true });
-
-    store.createIndex("statusIndex", "status");
+  const store = db.createObjectStore("transactionsStore", {
+    autoIncrement: true,
+  });
+  console.log(store);
+  store.createIndex("valueIndex", "value", { unique: false });
 };
 
-// Step - 2
-dbRequest.onsuccess = event => {
-    const db = event.target.result;
-    console.log(`Successfully opened connection to db: ${db.name}`);
+dbRequest.onsuccess = (event) => {
+  db = event.target.result;
+  console.log(`Successfully opened connection to db: ${db.name}`);
 };
 
-// Step - 3
-dbRequest.onerror = event => {
-    console.log(`error opening db: ${event.target.error}`);
+dbRequest.onerror = (event) => {
+  console.log(`error opening db: ${event.target.error}`);
 };
+
+const saveRecord = (transaction) => {
+  const tx = db.transaction("transactionsStore", "readwrite"),
+    store = tx.objectStore("transactionsStore");
+  console.log(`Post saved offline: ${JSON.stringify(transaction)}`);
+  console.log(navigator.onLine);
+
+  store.put(transaction);
+};
+
+const syncDB = () => {
+  console.log("sync");
+  console.log(navigator.onLine);
+
+  const tx = db.transaction("transactionsStore", "readwrite"),
+    store = tx.objectStore("transactionsStore");
+  const transactions = store.getAll();
+
+    transactions.onsuccess = function () {
+      if (transactions.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(transactions.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then(() => {
+            const transaction = db.transaction(
+              ["transactionsStore"],
+              "readwrite"
+            );
+
+            const store = transaction.objectStore("transactionsStore");
+
+            store.clear();
+          });
+      }
+    };
+};
+
+window.addEventListener("online", () => {
+  if (db !== undefined) {
+      console.log('from evt listener');
+      
+    syncDB();
+  }
+});
